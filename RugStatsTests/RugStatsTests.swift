16484a9e093a31875cd23f9ce9327bfa8e3928ec
k-isaka <isk.rugby@gmail.com>
2024-10-02 20:30:16 +0900

    Initial Commit

---
+++ RugStatsTests/RugStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RugStatsTests.swift
+//  RugStatsTests
+//
+//  Created by 井坂航 on 2024/10/02.
+//
+
+import Testing
+@testable import RugStats
+
+struct RugStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
