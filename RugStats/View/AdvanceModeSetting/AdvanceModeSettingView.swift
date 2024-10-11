1f829abc3db6230cef6d3b90521b979f92e91dd9
k-isaka <isk.rugby@gmail.com>
2024-10-11 22:34:10 +0900

    feat: チームプリセット一覧画面から新規作成画面を呼び出すように変更

--- RugStats/View/AdvanceModeSetting/AdvanceModeSettingView.swift
+++ RugStats/View/AdvanceModeSetting/AdvanceModeSettingView.swift
@@ -13,13 +13,11 @@ struct AdvanceModeSettingView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                NavigationLink("チームプリセット作成", destination: CreateTeamPresetView())
-                
                 // TODO: 遷移先画面を作成
                 NavigationLink("アクションプリセット作成", destination: EmptyView())
                 
                 // TODO: プリセット一覧画面を作成
-                NavigationLink("チームプリセット確認", destination: TeamPresetListView(teamList: $teamList))
+                NavigationLink("チームプリセット一覧", destination: TeamPresetListView(teamList: $teamList))
             }
         }
     }
---
+++ RugStats/View/AdvanceModeSetting/TeamPreset/TeamPresetListView.swift
@@ -0,0 +1,36 @@
+//
+//  TeamPresetListView.swift
+//  RugStats
+//
+//  Created by 井坂航 on 2024/10/10.
+//
+
+import SwiftUI
+
+struct TeamPresetListView: View {
+    @Binding var teamList: [TeamItem]
+    var body: some View {
+        NavigationStack{
+            VStack{
+                List($teamList, id:\.id){$team in
+                    NavigationLink(destination: {TeamPresetView(team: $team)}, label: {
+                        HStack{
+                            Text(team.name)
+                            Circle().fill(Color(CGColor(red: team.teamColor?.red ?? 0, green: team.teamColor?.green ?? 0, blue: team.teamColor?.blue ?? 0, alpha: team.teamColor?.alpha ?? 0))).frame(width: 20)
+                        }
+                    })
+                }
+            }.toolbar(content: {
+                ToolbarItem(placement: .bottomBar ,content: {
+                    Spacer()
+                    NavigationLink(destination: CreateTeamPresetView(), label: {
+                        Image(systemName: "square.and.pencil")
+                    })})
+            })
+        }
+    }
+}
+
+#Preview {
+    TeamPresetListView(teamList: .constant([TeamItem(name: "チーム")]))
+}
