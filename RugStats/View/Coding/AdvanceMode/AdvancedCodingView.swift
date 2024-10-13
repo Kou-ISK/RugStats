79937b481c7f2e526d74f55d1c5179897a6146ff
k-isaka <isk.rugby@gmail.com>
2024-10-13 22:38:19 +0900

    feat: アクションプリセット選択用のPickerを追加

--- RugStats/View/Coding/AdvanceMode/AdvancedCodingView.swift
+++ RugStats/View/Coding/AdvanceMode/AdvancedCodingView.swift
@@ -10,11 +10,43 @@ import SwiftUI
 struct AdvancedCodingView: View {
     @Binding var gameInfo: GameItem
     @Binding var gameClock: TimeInterval // ストップウォッチの経過時間
-    var actionPresetList: [ActionPresetItem]
+    @State var actionPresetList: [ActionPresetItem]
+    @State private var selectedActionPreset: ActionPresetItem
+    
+    init(gameInfo: Binding<GameItem>, gameClock: Binding<TimeInterval>, actionPresetList: [ActionPresetItem]) {
+        self._gameInfo = gameInfo
+        self._gameClock = gameClock
+        self.actionPresetList = actionPresetList
+        self._selectedActionPreset = State(initialValue: actionPresetList.first ?? ActionPresetItem(presetName: "デフォルト", actions: []))
+    }
     
     var body: some View {
         // TODO: 実装
-        Text("Hello, World!")
+        NavigationStack{
+            ForEach(selectedActionPreset.actions, id:\.id){action in
+                // TODO: 個人スタッツ用のボタンセットを実装
+                HStack{
+                    // TODO: コンポーネントを切り出して実装
+                    // TODO: 開始と終了の2回押せるようにする
+                    Button(action: {}, label: {Text("\(action.actionName)")})
+                    Button(action: {}, label: {Text("\(action.actionName)")})
+                }
+            }
+        }.toolbar{
+            ToolbarItem(placement: .automatic){
+                Picker("プリセット", selection: $selectedActionPreset) {
+                    ForEach(actionPresetList, id: \.self) { preset in
+                        Text(preset.presetName).tag(preset.presetName)
+                    }
+                }
+                .pickerStyle(.menu)
+                .onChange(of: selectedActionPreset) { oldPresetName, newPresetName in
+                    if let selectedPreset = actionPresetList.first(where: { $0.presetName == newPresetName.presetName}) {
+                        selectedActionPreset = selectedPreset
+                    }
+                }
+            }
+        }
     }
 }
 
